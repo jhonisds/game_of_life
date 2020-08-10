638eb81a56848c5e96879e613b595737d92cc6cb
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-08-10 19:37:47 -0300

    implement protocol petri_dish.

--- lib/game_of_life/world/binary.ex
+++ lib/game_of_life/world/binary.ex
@@ -3,4 +3,21 @@ defmodule GameofLife.World.Binary do
   Module binary
   """
   defstruct [:size, :cells]
+
+  @type t :: %__MODULE__{
+          size: pos_integer(),
+          cells: binary
+        }
+
+  def new(size) do
+    %__MODULE__{
+      size: size,
+      cells: init_cells(size)
+    }
+  end
+
+  defp init_cells(size) do
+    area = size * size
+    <<0::size(area)>>
+  end
 end
---
+++ lib/game_of_life/world/petri_dish.ex
@@ -0,0 +1,10 @@
+defprotocol GameOfLife.PretiDish do
+  @moduledoc """
+  Protocol Petri Dish
+  """
+  @spec activate(t, pos_integer(), pos_integer()) :: t
+  def activate(petri_dish, x, y)
+
+  @spec deactivate(t, pos_integer(), pos_integer()) :: t
+  def deactivate(petri_dish, x, y)
+end

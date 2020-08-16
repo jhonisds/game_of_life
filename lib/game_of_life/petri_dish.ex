210ab3ce401dd54e31a6b174d44d18a173b51e03
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-08-16 11:05:31 -0300

    fix protocol petri dish.

--- lib/game_of_life/petri_dish.ex
+++ lib/game_of_life/petri_dish.ex
@@ -6,18 +6,24 @@ defprotocol GameOfLife.PetriDish do
   @spec active?(t, pos_integer(), pos_integer()) :: boolean
   def active?(petri_dish, x, y)
 
-  @spec activate(t, pos_integer(), pos_integer()) :: t
-  def activate(petri_dish, x, y)
-
-  @spec deactivate(t, pos_integer(), pos_integer()) :: t
-  def deactivate(petri_dish, x, y)
+  def change_state(petri_dish, x, y, expected_state)
 
   @spec cells_to_analyze(t) :: [{pos_integer(), pos_integer()}]
-  def celss_to_analyze(petri_dish)
+  def cells_to_analyze(petri_dish)
 
   @spec active_neighbours(t, pos_integer(), pos_integer()) :: non_neg_integer()
   def active_neighbours(petri_dish, x, y)
 
   @spec clean(t) :: t
-  def clean(t) :: t
+  def clean(petri_dish)
+end
+
+defmodule State do
+  def activate(t, x, y) do
+    PetriDish.change_state(t, x, y, true)
+  end
+
+  def deactivate(t, x, y) do
+    PetriDish.change_state(t, x, y, false)
+  end
 end
